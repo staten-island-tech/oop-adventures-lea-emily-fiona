605f32f845ffe373aec6f3f411950c9ec1f8d899
Emily <emilycheng244@gmail.com>
2023-05-03 10:49:48 -0400

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,16 +1,20 @@
 class objects():
-    def __init__(self, thing):
-        self.thing = thing
-
-class weapon(objects):
-    def __init__(self, thing, use, name):
-        super().__init__(thing)
+    def __init__(self, name, use):
         self.name = name
         self.use = use
 
-class sword(weapon):
-    def __init__(self, thing, use, name, damage):
-        super().__init__(thing, use)
-        self.name = name
+class gold(objects):
+    def __init__(self, name, use):
+        self.name = "Gold"
+        self.use = "Purchase stuff"
+
+class weapon(objects):
+    def __init__(self, use, name, damage):
+        super()__init__(name)
+        self.use = "Damage enemies"
         self.damage = damage
+
+class sword(weapon):
+    def __init__(self, use, name, damage):
+        super()__init__(use, name, damage)
 
