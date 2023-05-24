496e51860d455098fd9500c5fe433b63d4ec0978
Emily <emilycheng244@gmail.com>
2023-05-24 10:46:31 -0400

    *did not mean to do this

--- objects.py
+++ objects.py
@@ -1,4 +1,7 @@
 from time import sleep
+longsleep = 2
+medsleep = 1.5
+shortsleep = 1
 class objects():
     def __init__(self, name, use):
         self.name = name
@@ -15,21 +18,21 @@ class gold(objects):
 class blacksmith_iron(objects):
     def __init__(self, name, use):
         super().__init__(name, use)
-        self.side_quest = sleep(2)
+        self.side_quest = sleep(longsleep)
         print("On your quest to find iron, you trip and fall")
         print("-1 life")
         lives = (lives-1)
-        sleep(2)
+        sleep(longsleep)
         print("However, you do end up finding iron, and the blacksmith is very grateful")
-        sleep(2)
+        sleep(longsleep)
         print("He gives you a sword and five pieces of gold to show his appreciation")
-        sleep(1.5)
+        sleep(medsleep)
         sword.name = "Iron sword"
         sword.damage = "40 damage"
         print("+1", sword.name)
-        sleep(1.5)
+        sleep(medsleep)
         print("+5 coins")
-        sleep(2)
+        sleep(longsleep)
         print("Your", sword.name, "does", sword.damage)
 
 class armor(objects):
@@ -73,27 +76,27 @@ class sleeping_villager(villager):
     def __init__(self, service):
         super().__init__(service)
         self.impact = print("You walk in, and the villager is sleeping")
-        sleep(2)
+        sleep(longsleep)
         print("While walking around, you step on a squeaky floorboard")
-        sleep(2)
+        sleep(longsleep)
         print("The noise awakens the villager and he punches you for interrupting his beauty sleep!")
-        sleep(1.5)
+        sleep(medsleep)
         print("-2 lives")
 class blacksmith(villager):
     def __init__(self, service, coins, items, impact):
         super().__init__(service, coins, items)
         self.impact = impact
         self.ask = print("The blacksmith asks for your help to find iron")
-        sleep(1)
+        sleep(shortsleep)
 class armorer(villager):
     def __init__(self, service, coins, items):
         super().__init__(service, coins, items)
         self.impact = print("+1", shield.name)
-        sleep(1.5)
+        sleep(medsleep)
         print("+1", armor.name)
-        sleep(1.5)
+        sleep(medsleep)
         print("The", shield.name, "gives you", shield.defense)
-        sleep(1.5)
+        sleep(medsleep)
         print("The", armor.name, "gives you", armor.defense)
         items.append("Iron shield")
         items.append("Iron armor")
@@ -101,11 +104,11 @@ class witch(villager):
     def __init__(self, service, coins, items):
         super().__init__(service, coins, items, lives)
         self.surprise = print("To your surprise, this witch is a good witch!")
-        sleep(2)
+        sleep(longsleep)
         self.items = potions
         potions.name = "healing potion"
         potions.use = "your lives get restored!"
         self.impact = print("She gives you a", potions.name, "and", potions.use)
-        sleep(1.5)
+        sleep(medsleep)
         print("reset : you now have 5 lives")
         lives = 5
--- oopgame.py
+++ oopgame.py
@@ -1,4 +1,4 @@
-from objects import *
+from classes import *
 lives = 5
 coins = []
 items = []
